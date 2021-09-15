4640b87edd252c86739169df1de191326cfd1d00
Amubasshir <themubasshir07@gmail.com>
2021-09-15 19:36:32 +0600

    updated

---
+++ 38.5-browser-api-practice/browserPractice.js
@@ -0,0 +1,34 @@
+// 38_5-1 Practice Browser api with checklist and a must work
+
+// 1. show output after 3.5 seconds
+console.log('Boreddddd');
+console.log('I need a tourrrr :)');
+setTimeout(() => console.log('aww jaiii *>_<*'), 3500);
+console.log('volagonj niya geleo colbeee :)');
+
+/* 2. take a number using prompt . add 200 . then alert the 
+ans: prompt.html & prompt.js
+3. do you want to see the href. if ok console log href
+ans: confirm.html & confirm.js
+4. purpose of cookies. 5-7 lines in 
+=> Cookies are created to identify you when you visit a new website. The web server — which stores the website's data — sends a short stream of identifying info to your web browser. Browser cookies are identified and read by “name-value” pairs. These tell cookies where to be sent and what data to recall.
+There are three types of computer cookies: session, persistent, and third-party. These virtually invisible text files are all very different. Each with their own mission, these cookies are made to track, collect, and store any data that companies request.
+5. difference between local storage and session storage
+=> 1 . 
+*/
+
+// ---- HOMEWORK ----
+/*
+6. how javascript run ? 5-7 lines in english
+=> JavaScript is an interpreted language where code is explained and executed at the runtime. Additionally, we know that web browsers understand HTML and CSS and converting those languages into a visual display on the screen. The part of the web browser that understands HTML and CSS is called the rendering engine. However, most browsers also have a JavaScript interpreter. That’s the part of the browser that understands JavaScript and run JavaScript programs.
+the browser's Rendering engine interacts with the "JavaScript's Interpreter" and sends the JavaScript code to the JavaScript engine for processing.
+The JavaScript engine/JavaScript Interpreter comprises both an Interpreter and a Just in Time compiler. Additionally, the overall execution of the JavaScript is a 5 step process,
+1. JavaScript Engine Loads Source Code.
+2. The interpreter starts the application.
+3. The Compiler receives code.
+4. The compiler starts optimization and compilation.
+5. The compiler incrementally optimizes the code.
+7. list of things that are asynchronous in js/ browser
+=>
+*/
+// 8. what is event loop in js and how does it work?
